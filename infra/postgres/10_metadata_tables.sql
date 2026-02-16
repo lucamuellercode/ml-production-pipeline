1c7ab50355378f3a2e0d5161e76ad9ccdd066b62
lucamuellercode <lucamueller.osk@googlemail.com>
2026-02-16 15:52:16 +0100

    added metadata table to track loaded version of the dataset

---
+++ infra/postgres/10_metadata_tables.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXIST metadata.datasets (
+    id BIGSERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    version TEXT NOT NULL,
+    source_uri TEXT NOT NULL,
+    row_count INTEGER,
+    loaded_at TIMESTAMP NOT NULL DEFAULT now(),
+    UNIQUE (name, version)
+)
